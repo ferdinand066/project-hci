9b14f820fe07eb333997f8c3338a105c07c36dc8
Danielfujiono <danielfujiono61@gmail.com>
2021-06-05 14:05:05 +0700

    testimoni & album

---
+++ HCI/js/album.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+
+    for (i = 0; i < 2; i++) {
+        $('.title')[i].onclick = function (e) {
+            var a = e.srcElement.parentElement.children[1].children[0]
+            if (a.className == 'artists nonactive') {
+                resetClassName()
+                a.className = a.className.replace('nonactive', 'active')
+                var cn = a.className.split(' ').join('.')
+                $('.artists.nonactive').slideUp(300)
+                $('.' + cn).slideDown(300)
+            }
+            else {
+                a.className = 'artists nonactive'
+                $('.artists.nonactive').slideUp(300)
+            }
+        }
+    }
+})
+
+function resetClassName() {
+    for (j = 0; j < 2; j++) {
+        var m = $('.artists')[j]
+        m.className = "artists nonactive"
+    }
+}
